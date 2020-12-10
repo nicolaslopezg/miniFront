aa86d9a343600238e367e62f53ae6f56cb5e84c8
Sebastián Salazar <ssalazar@option.cl>
2020-12-10 01:18:00 -0300

    Initial commit
    Signed-off-by: Sebastián Salazar <ssalazar@option.cl>

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,33 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+
+import SocketIO from 'socket.io-client';
+
+export default {
+  name: 'About',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      // clientSocket: SocketIO('http://67.205.168.127:3001/clients/clientsQueue', {
+      clientSocket: SocketIO('http://localhost:3001/clients/clientsQueue', {
+        transports: ['websocket'],
+        useConnectionNamespace: true,
+      }),
+    };
+  },
+  mounted() {
+    this.clientSocket.emit('client-request', {
+      fullName: 'Sebastián Salazar',
+      dni: '22.805.902-1',
+      email: 'seba.salazar.vivanco@gmail.com',
+      phoneNumber: '+56 962261873',
+    });
+  },
+};
+
+</script>
